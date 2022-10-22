12dee840f8fad219c9864f22ef323f93633a7e40
joyce0103 <115905733+joyce0103@users.noreply.github.com>
2022-10-22 00:19:25 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,8 @@
 terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
-   organization = "aliu"
+    # organization = "<replace-with-your-Terraform-Cloud-organization-and-uncomment>"
+    organization = "aliu"
     workspaces {
       name = "ace-iac-day-two"
     }
